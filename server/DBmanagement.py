7ca0f0e8ecbcb07a76a8876d2a364de1ba9db513
LChaeHyun <good4170@gmail.com>
2024-05-29 18:56:46 +0900

    db관리 추가

---
+++ server/DBmanagement.py
@@ -0,0 +1,63 @@
+import pymysql
+
+class Management:
+    def __init__(self):
+        self.conn = pymysql.connect(host='127.0.0.1', user='root', password='root', db='hunet', charset='utf8')
+        
+    #로그인 확인
+    def login(self,id,pwd):
+        cur = self.conn.cursor()
+        cur.execute("""select pwd from manager where id = %s;""",id)
+        a = cur.fetchone()
+        if(a[0] == pwd):
+            return True
+        else :
+            return False
+    
+    #rtsp:
+
+    #등록
+    def rtsp_add(self,name,address):
+        cur = self.conn.cursor()
+        query = """insert into rtsp (name, ip_address) values (%s, %s);"""
+        values = (name,address)
+        cur.execute(query,values)
+        self.conn.commit()
+
+    #삭제
+    def rtsp_delete(self,num):
+        cur = self.conn.cursor()
+        query = """delete from rtsp where id = %s;"""
+        cur.execute(query,num)
+        self.conn.commit()
+        
+    #읽기
+    def rtsp_get(self):
+        cur = self.conn.cursor()
+        cur.execute("""select * from rtsp;""")
+        return cur.fetchall()
+
+    #이메일 
+    #추가
+    def email_add(self,email):
+        cur = self.conn.cursor()
+        query = """insert into email (address) values (%s);"""
+        cur.execute(query,email)
+        self.conn.commit()
+    
+    #확인
+    def email_get(self):
+        cur = self.conn.cursor()
+        cur.execute("""select * from email;""")
+        return cur.fetchall()
+    
+       
+        
+
+
+
+
+
+
+
+
