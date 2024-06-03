1646c3f9c96a8ccedf11f869da6005166194ea4f
LChaeHyun <good4170@gmail.com>
2024-06-03 20:03:43 +0900

    데이터 추가 시 중복검사, 생성자 parameter 추가

--- server/DBmanagement.py
+++ server/DBmanagement.py
@@ -1,14 +1,18 @@
 import pymysql
 
 class Management:
-    def __init__(self):
-        self.conn = pymysql.connect(host='127.0.0.1', user='root', password='root', db='hunet', charset='utf8')
+    def __init__(self,h,u,p):
+        self.conn = pymysql.connect(host=h, user=u, password=p, db='hunet', charset='utf8')
 
     #사용자 추가
     def add_user(self,id,pwd):
         cur = self.conn.cursor()
-        cur.execute("""insert into manager (id, pwd) values (%s,%s);""",(id,pwd))
-        self.conn.commit()
+        cur.execute("""select * from manager where id = %s;""",id)
+        if cur.fetchone() == None:
+            cur.execute("""insert into manager (id, pwd) values (%s,%s);""",(id,pwd))
+            self.conn.commit()
+            return True
+        return False
    
         
     #사용자 확인(db_manager가 비었는지 확인, 비었으면 True) 
@@ -36,15 +40,17 @@ class Management:
     # 등록
     def rtsp_add(self,name,address):
         cur = self.conn.cursor()
-        query = """insert into rtsp (name, ip_address) values (%s, %s);"""
-        values = (name,address)
-        cur.execute(query,values)
-        self.conn.commit()
-
-        last_id = cur.fetchone()[0]
-        new_entry = {"id": last_id, "name": name, "ip_address": address}
-
-        return new_entry
+        cur.execute("""select * from rtsp where ip_address = %s;""",address)
+        if cur.fetchone() == None:
+            query = """insert into rtsp (name, ip_address) values (%s, %s);"""
+            values = (name,address)
+            cur.execute(query,values)
+            self.conn.commit()
+            last_id = cur.lastrowid
+            new_entry = {"id": last_id, "name": name, "ip_address": address}
+            return new_entry
+        
+        return False
 
     # 삭제
     def rtsp_delete(self,num):
@@ -59,13 +65,18 @@ class Management:
         cur.execute("""select * from rtsp;""")
         return cur.fetchall()
 
-    # 이메일
+    # 이메일:
+
     # 추가
     def email_add(self,email):
         cur = self.conn.cursor()
-        query = """insert into email (address) values (%s);"""
-        cur.execute(query,email)
-        self.conn.commit()
+        cur.execute("""select * from email where address = %s""",email)
+        if cur.fetchone() == None:
+            query = """insert into email (address) values (%s);"""
+            cur.execute(query,email)
+            self.conn.commit()
+            return True
+        return False
 
     # 확인
     def email_get(self):
@@ -73,14 +84,13 @@ class Management:
         cur.execute("""select * from email;""")
         return cur.fetchall()
 
-        # 삭제
-
+    # 삭제
     def email_delete(self, num):
         cur = self.conn.cursor()
         query = """delete from email where id = %s;"""
         cur.execute(query, num)
         self.conn.commit()
     
-    def refresh_connection(self):
+    def refresh_connection(self,h,u,p):
         self.conn.close()
-        self.__init__()
+        self.__init__(h,u,p)
